a23c376bd5af3e50f97b012a9fb7675eb4722673
Debraj-Das <debrajdas.gayatri@gmail.com>
2025-08-05 01:56:40 +0530

    update the hosting allow

--- admin/settings.py
+++ admin/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-3-8eb_ex)q=gw=_aso-@g8=w1)hw=dt*#1#@hh1gi3zu()4j##
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
